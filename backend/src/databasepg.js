d4a81bb2253aba3cc20de88baf1342d505ebe433
valdoryon <valdoryon@gmail.com>
2024-01-10 19:23:09 -0300

    server

--- backend/src/databasepg.js
+++ backend/src/databasepg.js
@@ -1,11 +1,20 @@
 const { Client } = require('pg');
 
 const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
+  ssl: true,
+});
+
+module.exports = { client };
+
+/*const client = new Client({
   host: 'localhost',
   user: 'postgres',
   port: 5473,
   password: '$valdomeiro$',
   database: 'SP_db',
-});
-
-module.exports = { client };
+});*/
