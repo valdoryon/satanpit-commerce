310b4434210ef5899d4e7c8261fd1c074c4de2d7
valdoryon <valdoryon@gmail.com>
2024-01-11 12:22:49 -0300

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,8 @@ require('dotenv').config({
 });
 
 //Importing middlewares
-const authenticateApiKey = require('../src/middleware/AuthenticateApiKey');
-const { client } = require('../src/databasepg');
+const authenticateApiKey = require('./middleware/AuthenticateApiKey');
+const { client } = require('./databasepg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -109,11 +109,10 @@ app.get('/v1/product/:product', authenticateApiKey, async (req, res) => {
   }
 });
 
+module.exports = app;
 // Serve static files from the 'backend' folder. images
 //app.use('/images', express.static('backend/images'));
 
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
-
-module.exports = app;
